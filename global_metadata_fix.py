9f538d7c83b3a325ddc4e5f9bf3482cd46cbf65f
CursedAhhhhh <mjovan5711@gmail.com>
2023-10-06 22:13:05 +0200

    got it decrypting

--- global_metadata_fix.py
+++ global_metadata_fix.py
@@ -1,4 +1,15 @@
 import struct
+import xxtea
+
+ENCRYTPED_KEY = b"##%$vsw'lytyqlusxul##p\"lvxrsv\"y\"y'xu%tv\"qsy\"l%%#qlux'ul# wylys\"t 'v$us''A"
+XOR_KEY = b"\x41"
+
+
+def xor_decrypt(ciphertext, key):
+    plaintext = bytearray(len(ciphertext))
+    for i in range(len(ciphertext)):
+        plaintext[i] = ciphertext[i] ^ key[i % len(key)]
+    return bytes(plaintext)
 
 def offset_count_pair(offset, count):
     print("Found Offset + Count: ", offset, count)
@@ -43,13 +54,32 @@ def find_offset_count_pair(offset: int):
             offset_count_pair(offset, closest_count)
             if find_offset_count_pair(closest_offset):
                 return True
-            
+     
     return False
 
+def decrypt_header():
+    with open('global-metadata.dat', 'rb') as file:
+        global_metadata = file.read()
+        
+    key = xor_decrypt(ENCRYTPED_KEY, XOR_KEY)[:16]
+    header_offset = int.from_bytes(global_metadata[56:60], byteorder='little')
+    header_size = int.from_bytes(global_metadata[60:64], byteorder='little')
+    print("Header Offset: " + str(header_offset))
+    print("Header Size: " + str(header_size))
 
-# Define the format for a single int32_t value (4 bytes)
-with open('metadataheader.dat', 'rb') as file:
-    binary_data = file.read(256)
+    #We use the header offset to find beggining of encrypted header, and the size to find the end
+    encrypted_header = global_metadata[header_offset:header_offset+header_size]
+    decrypted_header = xxtea.decrypt(encrypted_header, key, padding=False)
+    #weird for some reason the size of the header is maintained, so we remove it
+    decrypted_header = decrypted_header[:-4]
+
+    print("SUCCESS!")
+    print(decrypted_header)
+    return decrypted_header
+
+
+# decrypt the header and convert it to a list of int_32's
+binary_data = decrypt_header()
 incorrect_order = list(struct.unpack('i' * (len(binary_data) // struct.calcsize('i')), binary_data))
 
 #checking for sanity and version
@@ -71,6 +101,7 @@ else:
 
 string_offset = min([x for x in incorrect_order if x != 0])
 correct_order = [sanity, version]
+print(string_offset)
 
 success = find_offset_count_pair(string_offset)
 if not success:
@@ -79,7 +110,13 @@ if not success:
 else:
     print("Success!")
 
-with open('fixed_metadataheader.dat', 'wb') as file:
+with open('global-metadata.dat', 'rb') as file:
+        file_data = file.read()
+
+with open('fixed-global-metadata.dat', 'wb') as file:
     for value in correct_order:
         packed_value = struct.pack('i', value)
         file.write(packed_value)
+    packed_value = struct.pack('i', 256)
+    file.write(packed_value)
+    file.write(file_data[260:])
